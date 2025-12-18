25ca8818e04422ba1be086a3aab3deddd0919cda
Zelfaesque <fiona.fauv@gmail.com>
2025-12-18 08:15:07 +0100

    Update : Style CSS

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,8 +1,10 @@
 import FormSection from '@/components/auth/FormSection';
-
+import { MeruBackground } from '@/components/ui/Merubackground';
 
 export default function LoginPage() {
   return (
+    <MeruBackground>
     <FormSection />
+    </MeruBackground>
   );
 }
