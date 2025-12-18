ce4d5d3595472fd4ed6f064c78f0fb05b1f1d2f4
Zelfaesque <fiona.fauv@gmail.com>
2025-12-18 11:17:52 +0100

    New : Constants Faqs

---
+++ src/lib/constants/faqs.tsx
@@ -0,0 +1,20 @@
+  const faqs = [
+    {
+      question: 'Comment créer un compte ?',
+      answer: 'Cliquez sur le bouton "Inscription" en haut à droite, remplissez le formulaire et validez votre email.',
+    },
+    {
+      question: 'Comment rejoindre un tournoi ?',
+      answer: 'Consultez la liste des tournois disponibles et cliquez sur "S\'inscrire" sur celui de votre choix.',
+    },
+    {
+      question: 'Les tournois sont-ils gratuits ?',
+      answer: 'Certains tournois sont gratuits, d\'autres peuvent nécessiter des frais d\'inscription. Vérifiez les détails.',
+    },
+    {
+      question: 'Comment devenir partenaire ?',
+      answer: 'Contactez-nous via le formulaire en sélectionnant "Partenariat" comme sujet de votre message.',
+    },
+  ];
+
+  export { faqs };
