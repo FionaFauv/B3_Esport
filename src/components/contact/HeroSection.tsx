fec49ff8ec7a2fa3774d1d74f4d1b52696556648
Zelfaesque <fiona.fauv@gmail.com>
2025-12-24 20:56:22 +0100

    Update : Particules retirer

--- src/components/contact/HeroSection.tsx
+++ src/components/contact/HeroSection.tsx
@@ -1,23 +1,14 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Section } from '@/components/ui/section'
 import { Container } from '@/components/ui/container'
-import { useTheme } from 'next-themes'
-import { Particles } from '../ui/particles'
 
 /**
  * Hero Section de la page Contact
  * Affiche le titre et la description de la page
  */
 export default function HeroSection() {
-
-  const { theme } = useTheme()
-  const [color, setColor] = useState("#401717ff")
-
-  useEffect(() => {
-    setColor(theme === "dark" ? "#e8c6c6ff" : "#531c1cff")
-  }, [theme])
 
   return (
     <Section paddingY="large">
@@ -29,13 +20,6 @@ export default function HeroSection() {
           Une question, une suggestion ou besoin d&apos;aide ? Notre équipe est là pour vous répondre.
         </p>
       </Container>
-      <Particles
-        className="absolute inset-0"
-        quantity={100}
-        ease={80}
-        color={color}
-        refresh
-      />
     </Section>
   )
 }
