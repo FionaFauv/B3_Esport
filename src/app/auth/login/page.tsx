6daf6f40e8f6ad258d2ce116dafedbf3043f342a
Zelfaesque <fiona.fauv@gmail.com>
2025-12-24 20:56:06 +0100

    Update : Ajout du Metadata

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,10 @@
 import FormSection from '@/components/auth/FormSection';
-
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Connexion',
+  description: 'Connectez-vous à votre compte Paris Esportif.',
+}
 export default function LoginPage() {
   return (
     <FormSection />
