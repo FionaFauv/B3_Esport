b864084a65974f4ee12a1c6ab1e91540c11bcdfc
Zelfaesque <fiona.fauv@gmail.com>
2026-01-05 17:04:38 +0100

    Update : correction anticancelled

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,8 +1,8 @@
 import PocketBase from 'pocketbase';
 
 const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL);
-
 // Log pour déboguer l'URL
 console.log('PocketBase URL:', process.env.NEXT_PUBLIC_PB_URL);
+pb.autoCancellation(false);
 
 export { pb };
